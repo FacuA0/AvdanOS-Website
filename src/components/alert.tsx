dbc571b6d90c8d462905986c35fb34c21a06069f
FacuA0 <facundoamarilla28@gmail.com>
2022-06-30 16:07:43 -0300

    Centered Discord alert

--- src/components/alert.tsx
+++ src/components/alert.tsx
@@ -6,28 +6,30 @@ const Alert = () => {
   const theme = useTheme();
   return (
     <>
-      <div style={{ display: 'flex', justifyContent: 'center', zIndex: 4, position: 'relative' }}>
-        <Grid
-          style={{
-            paddingInline: 50,
-            pointerEvents: 'none',
-            display: 'block',
-            width: '100%'
-          }}
-        >
-          <Card className={style.card + ' ' + (theme.type == 'dark' ? style.card__dark : style.card__white)}>
-            <p style={{ fontSize: 14, margin: -10, marginLeft: 0 }}>
-              <strong>
-                <strong style={{ fontWeight: 400 }}>AvdanOS</strong>{' '}
-                <TranslatableText>is still in development.</TranslatableText>
-              </strong>{' '}
-              <Link style={{ pointerEvents: 'stroke' }} block underline href="https://discord.gg/avdanos">
-                <TranslatableText>Join our Discord</TranslatableText>
-              </Link>
-            </p>
-          </Card>
-        </Grid>
-      </div>
+	  <div style={{ textAlign: 'center' }}>
+	    <div style={{ display: 'inline-block', justifyContent: 'center', zIndex: 4, position: 'relative' }}>
+	      <Grid
+		    style={{
+		  	  paddingInline: 50,
+			  pointerEvents: 'none',
+			  display: 'block',
+			  width: '100%'
+		    }}
+		  >
+		    <Card className={style.card + ' ' + (theme.type == 'dark' ? style.card__dark : style.card__white)}>
+			  <p style={{ fontSize: 14, margin: -10, marginLeft: 0 }}>
+			    <strong>
+				  <strong style={{ fontWeight: 400 }}>AvdanOS</strong>{' '}
+				  <TranslatableText>is still in development.</TranslatableText>
+			    </strong>{' '}
+			    <Link style={{ pointerEvents: 'stroke' }} block underline href="https://discord.gg/avdanos">
+				  <TranslatableText>Join our Discord</TranslatableText>
+			    </Link>
+			  </p>
+		    </Card>
+		  </Grid>
+	    </div>
+	  </div>
     </>
   );
 };
